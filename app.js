5378bfb3240a6f3c25c02f2311abb000b815b691
EryouHao <haoeryou@qq.com>
2019-12-08 08:34:11 +0800

    update template

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express()
 app.use(express.static(__dirname))
 
 app.set('views', path.join(__dirname, '/templates'));
-app.set('view engine', 'html');
-app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+// app.engine('html', ejs.renderFile);
 
 /**
  * Home Page & Post List Page
