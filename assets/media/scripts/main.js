cb05d8135d5e2771713b2f7b57b28682239cc5f7
EryouHao <eryouhao@gmail.com>
2019-12-09 19:59:02 +0800

    update docs template

--- assets/media/scripts/main.js
+++ assets/media/scripts/main.js
@@ -3,50 +3,6 @@ new Vue({
   data() {
     return {
       isOpen: false,
-      popularDestinations: [
-        {
-          city: 'Toronto',
-          averagePrice: 120,
-          propertyCount: 76,
-          imageUrl: '/static/gridea.png',
-          imageAlt: 'Toronto skyline',
-        },
-        {
-          city: 'Malibu',
-          averagePrice: 215,
-          propertyCount: 43,
-          imageUrl: '/static/gridea.png',
-          imageAlt: 'Cliff in Malibu',
-        },
-        {
-          city: 'Chicago',
-          averagePrice: 130,
-          propertyCount: 115,
-          imageUrl: '/static/gridea.png',
-          imageAlt: 'Chicago skyline',
-        },
-        {
-          city: 'Seattle',
-          averagePrice: 135,
-          propertyCount: 63,
-          imageUrl: '/static/gridea.png',
-          imageAlt: 'Seattle skyline',
-        },
-        {
-          city: 'Colorado',
-          averagePrice: 85,
-          propertyCount: 47,
-          imageUrl: '/static/gridea.png',
-          imageAlt: 'Lake in Colorado',
-        },
-        {
-          city: 'Miami',
-          averagePrice: 115,
-          propertyCount: 86,
-          imageUrl: '/static/gridea.png',
-          imageAlt: 'Beach in Miami',
-        },
-      ]
-    }
+    } 
   },
 })
